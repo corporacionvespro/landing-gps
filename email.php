16ffbe9e2c8e4b2d5b4c342856a4d663aa5668fc
el-bryant <bry.y.valen@gmail.com>
2020-02-21 11:23:43 -0500

    contraseña

--- email.php
+++ email.php
@@ -11,7 +11,7 @@ require 'vendor/autoload.php';
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-$pdo = new PDO("mysql:host=localhost;dbname=landing;charset=utf8", "root", "", array(
+$pdo = new PDO("mysql:host=localhost;dbname=landing;charset=utf8", "root", "palomino2011", array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_EMULATE_PREPARES => false
 ));
