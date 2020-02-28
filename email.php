2cef6972cbbb64ff24c28c666b9abcdd71d6adde
el-bryant <bry.y.valen@gmail.com>
2020-02-28 12:17:00 -0500

    funcionando

--- email.php
+++ email.php
@@ -31,59 +31,56 @@ try{
     $result = $pdo->prepare($query);
     $exec = $result->execute();
     
-    // try {
+    try {
        
-    //     //Server settings
-    //     $mail->SMTPDebug = 0;                                       // Enable verbose debug output
-    //     $mail->isSMTP();                                            // Set mailer to use SMTP
-    //     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    //     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    //     $mail->Username   = 'tecnologiavespro@gmail.com';                     // SMTP username
-    //     $mail->Password   = 'jacmegzeisdcooep';                               // SMP passwordT
-    //     $mail->SMTPSecure = 'SMTP';                                  // Enable TLS encryption, `ssl` also accepted
-    //     // $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    //     // $mail->Port       = 587;                                    // TCP port to connect to
-    //     // $mail->Port       = 465;                                    // TCP port to connect to
-    //     $mail->Port       = 25;                                    // TCP port to connect to
+        //Server settings
+        $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+        $mail->isSMTP();                                            // Set mailer to use SMTP
+        $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'tecnologiavespro@gmail.com';                     // SMTP username
+        $mail->Password   = 'jacmegzeisdcooep';                               // SMP passwordT
+        $mail->SMTPSecure = 'SMTP';                                  // Enable TLS encryption, `ssl` also accepted
+        // $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+        // $mail->Port       = 587;                                    // TCP port to connect to
+        // $mail->Port       = 465;                                    // TCP port to connect to
+        $mail->Port       = 25;                                    // TCP port to connect to
     
-    //     //Recipients
-    //     $mail->setFrom('tecnologiavespro@gmail.com', 'Sistemas GPS');
-    //     $mail->addAddress('tecnologiavespro@gmail.com');     // Add a recipient
+        //Recipients
+        $mail->setFrom('tecnologiavespro@gmail.com', 'Sistemas GPS');
+        $mail->addAddress('tecnologiavespro@gmail.com');     // Add a recipient
     
-    //     // Content
-    //     $mail->isHTML(true);                                  // Set email format to HTML
-    //     $mail->Subject = 'GPS: '.$nombres;
-    //     $mail->Body    = '<b>Nombre:</b> '.$nombres.'<br>';
-    //     $mail->Body    = $mail->Body.'<b>Empresa:</b> '.$empresa.'<br>';
-    //     $mail->Body    = $mail->Body.'<b>Telefono:</b> '.$whatsapp.'<br>';
-    //     $mail->Body    = $mail->Body.'<b>Email:</b> '.$correo.'<br>';
-    //     $mail->Body    = $mail->Body.'<b>Codigo Promoción:</b> '.$codigo.'<br>';
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'GPS: '.$nombres;
+        $mail->Body    = '<b>Nombre:</b> '.$nombres.'<br>';
+        $mail->Body    = $mail->Body.'<b>Empresa:</b> '.$empresa.'<br>';
+        $mail->Body    = $mail->Body.'<b>Telefono:</b> '.$whatsapp.'<br>';
+        $mail->Body    = $mail->Body.'<b>Email:</b> '.$correo.'<br>';
+        $mail->Body    = $mail->Body.'<b>Codigo Promoción:</b> '.$codigo.'<br>';
        
-    //     header('Content-Type: application/json');    
-    //     if (!$mail->send()) {
-    //         $pdo->rollBack();
-    //         echo json_encode([
-    //             "status"    => "ERROR",
-    //             "data"      => $mail->ErrorInfo
-    //         ]);
-    //     }else {
-    //         $pdo->commit();	
-    //         echo json_encode([
-    //             "status"    => "OK",
-    //             "data"      => "Mensaje enviado."
-    //         ]);
-    //     }
-    // }catch (\Exception $e) {
-    //     $pdo->rollBack();
-    //     header('Content-Type: application/json');    
-    //     echo json_encode([
-    //         "status"    => "ERROR",
-    //         "data"      => $mail->ErrorInfo
-    //     ]);
-    // }
-
-    header('Location: https://api.whatsapp.com/send?phone=51997630413&text=Hola, soy '.$nombres.' de la empresa '.$empresa.', mi correo es '.$correo.' y solicito información sobre el GPS');
-
+        header('Content-Type: application/json');    
+        if (!$mail->send()) {
+            $pdo->rollBack();
+            echo json_encode([
+                "status"    => "ERROR",
+                "data"      => $mail->ErrorInfo
+            ]);
+        }else {
+            $pdo->commit();	
+            echo json_encode([
+                "status"    => "OK",
+                "data"      => "Mensaje enviado."
+            ]);
+        }
+    }catch (\Exception $e) {
+        $pdo->rollBack();
+        header('Content-Type: application/json');    
+        echo json_encode([
+            "status"    => "ERROR",
+            "data"      => $mail->ErrorInfo
+        ]);
+    }
 }catch(\Exception $e){
     header('Content-Type: application/json');    
     $pdo->rollBack();
